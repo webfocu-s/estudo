3c586ecff9a943f41a37e036fdea89a4103272b7
Fernando Pereira <prefix.geral@gmail.com>
2019-03-14 12:56:58 +0100

    14_03_2019

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,17 +1,81 @@
 import React, { Component } from 'react';
 
 import api from '../../services/api';
+import './style.css';
 
 export default class Main extends Component{
+
+    state = {
+        products : []
+    }
+
+    
     componentDidMount(){
      this.loadProdutos();
     }
 
     loadProdutos = async () => {
+
         const dados = await api.get('/products');
-        console.log(dados)
+        const { docs, ...productInfo } = dados.data;
+
+        this.setState( { products: docs, productInfo} );
+      
+       console.log('====================================');
+       console.log(this.state.products);
+       console.log('====================================');
+        
     }
+
+    nextPage = () => {
+      const {page, pageInfo} = this.state.products;
+
+     // if(page === pageInfo.pages) return;
+      console.log('====================================');
+      console.log(page);
+      console.log('====================================');
+    }
+    prevPage = () => {
+       
+    }
+
+  
+
    render(){
-       return <h1>Novo componente</h1>
+    const { products } = this.state;
+       return (
+       <div>
+       
+       { products.map( product => (
+
+
+            <a key={product._id} href={product.url} target="" id="article">
+            <div id="img_product"> 
+                <img alt="Culrs" src="https://ph-files.imgix.net/2980900f-ddbe-406b-a188-ed1f7fcc1d52?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop"/> 
+            </div>           
+
+            <div>
+            <h3>{product.title}</h3>
+            <p>{product.description}</p>
+            </div>
+            
+            </a>
+
+
+       ))}
+
+       <div id="actionButton">
+           <button onClick={this.prevPage}>Anterior</button>
+           <button onClick={this.nextPage}>Próximo</button>
+       </div>
+
+       </div>
+       
+       )
    }
+
 }
+
+
+
+   
